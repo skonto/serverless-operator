c662fd2d843a61c76cdea5ae995efce98eafd6b1
Stavros Kontopoulos <skontopo@redhat.com>
2020-09-18 14:57:45 +0300

    add more service monitors

--- knative-operator/pkg/common/service_monitor.go
+++ knative-operator/pkg/common/service_monitor.go
@@ -4,21 +4,23 @@ import (
 	"context"
 	"fmt"
 
-	"k8s.io/client-go/kubernetes/scheme"
+	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
 	mfclient "github.com/manifestival/controller-runtime-client"
 	mf "github.com/manifestival/manifestival"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	eventingv1alpha1 "knative.dev/operator/pkg/apis/operator/v1alpha1"
 	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	kubemetrics "github.com/operator-framework/operator-sdk/pkg/kube-metrics"
-	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
 	"github.com/operator-framework/operator-sdk/pkg/metrics"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	eventingv1alpha1 "knative.dev/operator/pkg/apis/operator/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
+
 const EventingBrokerServiceMoinitorPath = "deploy/resources/broker-service-monitors.yaml"
+const EventingPingSourceMoinitorPath = "deploy/resources/ping_source_service_monitor.yaml"
 
 func SetupServerlessOperatorServiceMonitor(ctx context.Context, cfg *rest.Config, api client.Client, metricsPort int32, metricsHost string, operatorMetricsPort int32) error {
 	// Commented below to avoid a stream of these errors at startup:
@@ -62,6 +64,10 @@ func SetupServerlessOperatorServiceMonitor(ctx context.Context, cfg *rest.Config
 // serveCRMetrics gets the Operator/CustomResource GVKs and generates metrics based on those types.
 // It serves those metrics on "http://metricsHost:operatorMetricsPort".
 func serveCRMetrics(cfg *rest.Config, metricsHost string, operatorMetricsPort int32) error {
+
+	// If we dont use a custom list here, the typical call to get a filtered list of gvks using k8sutil.GetGVKsFromAddToScheme(apis.AddToScheme)
+	// will end up with resources the Operator does not have access to eg. `Kind=LimitRange`
+	// The list of resources returned is unrelated to our purposes here, thus the customization.
 	gvkFilterList := []schema.GroupVersionKind{
 		schema.GroupVersionKind{
 			Group:   "operator.knative.dev",
@@ -69,54 +75,51 @@ func serveCRMetrics(cfg *rest.Config, metricsHost string, operatorMetricsPort in
 			Kind:    "KnativeServing",
 		},
 		schema.GroupVersionKind{
-			Group: "operator.knative.dev",
+			Group:   "operator.knative.dev",
 			Version: "v1alpha1",
-			Kind: "KnativeEventing",
+			Kind:    "KnativeEventing",
 		},
 	}
 	// To generate metrics in other namespaces, add the values below.
-	ns := []string{""}
+	// This is due to this bug: https://github.com/operator-framework/operator-sdk/issues/2494
+	// For the workaround check here: https://github.com/operator-framework/operator-sdk/pull/2601/files#r396745465
+	// and https://github.com/shipwright-io/build/pull/73
+	// In order to avoid getting a bad value we avoid using k8sutil.GetWatchNamespace() that gets the value from the WATCH_NAMESPACE
+	// env var. That value is by default "" but user may change it, affecting the metrics endpoint.
+	namespaces := []string{""}
 	// Generate and serve custom resource specific metrics.
-	err := kubemetrics.GenerateAndServeCRMetrics(cfg, ns, gvkFilterList, metricsHost, operatorMetricsPort)
+	err := kubemetrics.GenerateAndServeCRMetrics(cfg, namespaces, gvkFilterList, metricsHost, operatorMetricsPort)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func SetupEventingServiceMonitors(namespace string, instance *eventingv1alpha1.KnativeEventing)  error {
-	config, err := rest.InClusterConfig()
+func SetupEventingServiceMonitors(client client.Client, namespace string, instance *eventingv1alpha1.KnativeEventing) error {
+	manifest, err := mf.NewManifest(EventingBrokerServiceMoinitorPath, mf.UseClient(mfclient.NewClient(client)))
 	if err != nil {
-		return fmt.Errorf("failed to create cluster config: %w", err)
-	}
-	cl, err := client.New(config, client.Options{})
-	if err != nil {
-		return fmt.Errorf("failed to create client: %w", err)
-	}
-	manifest, err := mf.NewManifest(EventingBrokerServiceMoinitorPath, mf.UseClient(mfclient.NewClient(cl)))
-	if err != nil {
-		return fmt.Errorf("unable to parse broker services: %w", err)
+		return fmt.Errorf("unable to parse broker service monitors: %w", err)
 	}
 	transforms := []mf.Transformer{mf.InjectOwner(instance), mf.InjectNamespace(namespace)}
 	if manifest, err = manifest.Transform(transforms...); err != nil {
 		return fmt.Errorf("unable to transform broker service monitors manifest: %w", err)
 	}
-	mon := &monitoringv1.ServiceMonitor{}
-
+	// this is required because Apply will fail with not know resource later on
+	monitor := &monitoringv1.ServiceMonitor{}
 	var SchemeGroupVersion = schema.GroupVersion{Group: "monitoring.coreos.com", Version: "v1"}
-	scheme.Scheme.AddKnownTypes(SchemeGroupVersion, mon)
-
-	//if err := scheme.Scheme.Convert(&manifest.Resources()[0], mon, nil); err != nil {
-	//	return err
-	//}
+	scheme.Scheme.AddKnownTypes(SchemeGroupVersion, monitor)
 	if err := manifest.Apply(); err != nil {
-		//errr := cl.Create(context.Background(), mon)
-		//if errr != nil {
-		//	return fmt.Errorf("unable to create broker service monitors second %w ---> %w", errr, err)
-		//}
-		//return fmt.Errorf("unable to create broker service monitors %w $$$$$ %v", err, manifest)
+		return err
+	}
+	manifest, err = mf.NewManifest(EventingPingSourceMoinitorPath, mf.UseClient(mfclient.NewClient(client)))
+	if err != nil {
+		return fmt.Errorf("unable to parse ping source service monitor: %w", err)
+	}
+	if manifest, err = manifest.Transform(transforms...); err != nil {
+		return fmt.Errorf("unable to transform ping source service monitor manifest: %w", err)
+	}
+	if err := manifest.Apply(); err != nil {
 		return err
 	}
 	return nil
 }
-
